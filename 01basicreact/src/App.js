0e806af8b4a661a161f42fc235bf6fc7ec378a42
ChiragJalade27 <csjalade2002@gmail.com>
2023-10-06 23:40:58 +0530

    commit all

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>They love you</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai(){
+    return(
+        <h1>This is testing</h1>
+    )
+
+}
+
+export default Chai;
